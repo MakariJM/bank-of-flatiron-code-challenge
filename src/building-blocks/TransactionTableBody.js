9f9a60dc152967952863ade8bb5915f0215758a9
JuliusMakari <muragejmakari@gmail.com>
2024-02-05 02:17:36 +0300

    eighth commit

---
+++ src/building-blocks/TransactionTableBody.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Button from "./Button";
+
+const TransactionTableBody = ({
+  transactions,
+  onTransactionUpdate,
+  onTransactionDelete,
+}) => {
+  const transformedDate = (date) => {
+    return new Date(date).toDateString();
+  };
+  const colorAmountBelowZero = (amt) => {
+    if (amt < 0) {
+      return "negative-amount";
+    } else {
+      return "";
+    }
+  };
+
+  const transactionList =
+    transactions.length === 0 ? (
+      <tr>
+        <td colSpan="5">No records found!!!</td>
+      </tr>
+    ) : (
+      transactions.map((transaction) => {
+        return (
+          <tr key={transaction.id}>
+            <th scope="row">{transformedDate(transaction.date)}</th>
+            <td data-title="Description">{transaction.description}</td>
+            <td data-title="Category">{transaction.category}</td>
+            <td
+              data-title="amount"
+              data-type="currency"
+              className={colorAmountBelowZero(transaction.amount)}
+            >
+              {transaction.amount}
+            </td>
+            <td data-title="Manage Transaction">
+              <div>
+                <Button
+                  textContent={"Edit"}
+                  type={"button"}
+                  className={"btn btn-secondary btn-sm mr-2"}
+                  onAction={() => onTransactionUpdate(transaction.id)}
+                />
+                <Button
+                  textContent={"Delete"}
+                  type={"button"}
+                  className={"btn btn-danger btn-sm"}
+                  onAction={() => onTransactionDelete(transaction.id)}
+                />
+              </div>
+            </td>
+          </tr>
+        );
+      })
+    );
+  return <tbody>{transactionList}</tbody>;
+};
+
+export default TransactionTableBody;
